709280016695a72ea034d48c3353ffa70139f5d0
user.email <jobayer.ahmed109@gmail.com>
2024-09-30 22:03:36 +0600

    "Added gradients and basic structure to App component"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,19 @@
 function App() {
   return (
     <section className="bg-[#1A232E] relative h-screen py-6 sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle">
-      {/* Header */}
-      <div></div>
+      {/* Gradients */}
+      <div className="gradient-01 z-0 absolute"></div>
+      <div className="gradient-02 z-0 absolute"></div>
 
+      {/* Header */}
+      <div className="text-3xl font-bold tracking-wider uppercase text-white text-center opacity-90 mb-4">
+        ChatGPT 2.0
+      </div>
       {/* Body */}
-      <div></div>
+      <div>Body</div>
 
       {/* Input */}
-      <div></div>
+      <div>Input</div>
     </section>
   );
 }
