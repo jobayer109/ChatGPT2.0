dffc8ca6cfbfc1616c509ccf0c602ee271ac13af
user.email <jobayer.ahmed109@gmail.com>
2024-09-30 21:15:17 +0600

    Intial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+// import "./App.css";
+
+function App() {
+  return (
+    <section>
+      {/* Header */}
+      <div></div>
+
+      {/* Body */}
+      <div></div>
+
+      {/* Input */}
+      <div></div>
+    </section>
+  );
+}
+
+export default App;
